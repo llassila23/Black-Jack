5243ede1d2776895db80bf39cce7bfc2ec70b6d0
llassila23 <129896722+llassila23@users.noreply.github.com>
2023-04-20 10:58:58 -0700

    outline methods and functions

---
+++ LL Card Game/CardMain.java
@@ -0,0 +1,18 @@
+// the Card Main class will execute the program by calling the methods defined in Card Method
+
+public class CardMain{ 
+    // execute my program
+    public static void main(String[]args){
+// create new instance of cardMethod
+// begin calling methods to act on the card class
+        // print instructions (file I/O)
+        //shuffle
+        // deal (display to terminal)
+        // ** if possible** deal to computer too, play against computer
+        // ** points system, could wager
+        // logic operation
+        // deal again or compare hands
+        //** assign points to player
+        // ask if want to play again
+    }
+}
---
+++ LL Card Game/CardMethod.java
@@ -0,0 +1,25 @@
+// the Card Method class will have the methods that will be called by Card Main and act on the Card Class
+// object
+// I will choose the game 21 (aka black jack) because I know it best. Being a simpler game I may 
+// be able to add more funtionality to my program rather than working out the nuances of the game itself.
+
+public class CardMethod{
+    // ints: card counter, points counter
+    // file IO for instructions
+    // start with 10 points
+    // randomizer for the shuffle function
+    // two arrays, one for face one for suite
+    // use arrays to create a card by taking an element from suite and face arrays
+    // put the cards into a new array
+    // shuffle them using the random number, array reversal ish but with rand num for index
+    // put this shuffled array into a stack
+    // ** offer wagers
+    // array list for the ** computer and the player
+    // deal the 2 cards per player
+    // only print the users array list
+    // ** logic operation for computer
+    // offer a hit
+    // show cards and sum them
+    // ** assign points back
+    // offer to play again
+}
